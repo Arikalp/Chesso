26cf9fa33b652d905d7fe6780bbf4c380ef08d3d
Arikalp <sankalpkrsaini@gmail.com>
2026-03-06 15:58:30 +0530

    Chat msg distinction rewrite

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,18 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Developed by Sankalp with ❤️</p>
+      <p>
+        Developed by{" "}
+        <a 
+          href="https://arikalp.vercel.app" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className={styles.gradientLink}
+        >
+          Sankalp
+        </a>{" "}
+        with ❤️
+      </p>
     </footer>
   );
 }
